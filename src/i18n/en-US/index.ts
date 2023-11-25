22f66f4d14b2a2c3d2baa84bc0d24d29fce6a29d
Ojvar <mojvar.amirhossein@gmail.com>
2023-11-25 13:27:08 +0330

    init project

---
+++ src/i18n/en-US/index.ts
@@ -0,0 +1,8 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  welcome: 'Welcome',
+  failed: 'Action failed',
+  success: 'Action was successful'
+};
---
+++ src/i18n/fa-IR/index.ts
@@ -0,0 +1,8 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+    welcome: 'خوش آمدید',
+  failed: 'شکست در عملیات',
+  success: 'موفقیت در عملیات',
+};
